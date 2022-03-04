bac213b33eb224022241c265aa64278896c41505
Niamat <niamatullah.akbari@opp.eduvantaa.fi>
2022-03-04 11:27:28 +0200

    learning array

--- src/index.js
+++ src/index.js
@@ -2,30 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  const phonebook = {
-    name: 'Superadvanced phonebook app',
-    contacts: [
-      {
-        name: 'John Doe',
-        phonenumber: '358401234567',
-        id: 1
-      },
-      {
-        name: 'Jane Doe',
-        phonenumber: '44551234567',
-        id: 2
-      },
-      {
-        name: 'Foo bar',
-        phonenumber: '000',
-        id: 3
-      }
-    ]
-  }
+  const title = 'Superadvanced web phonebook app'
+  const contacts = [
+    {
+      name: 'John Doe',
+      phonenumber: '358401234567'
+    },
+    {
+      name: 'Jane Doe',
+      phonenumber: '44551234567'
+    },
+    {
+      name: 'Foo bar',
+      phonenumber: '000'
+    }
+  ]
 
   return (
     <div>
-      <phonebook phonebook={phonebook} />
+      <header title={title} />
+      <contents contacts={contacts} />
     </div>
   )
 }
@@ -35,6 +31,7 @@ ReactDOM.render(
   <App />,
   document.getElementById('root')
 )
+
 
 
 

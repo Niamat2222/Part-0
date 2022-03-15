88a85b6ae4cfc717b30960d71d435d76b7449863
Niamat <niamatullah.akbari@opp.eduvantaa.fi>
2022-03-15 13:28:14 +0200

    Number of entries

--- src/index.js
+++ src/index.js
@@ -1,27 +1,46 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './index.css';
 
 const App = () => {
-  const title = 'Superadvanced web phonebook app'
-  const contacts = [
-    {
-      name: 'John Doe',
-      phonenumber: '358401234567'
-    },
-    {
-      name: 'Jane Doe',
-      phonenumber: '44551234567'
-    },
-    {
-      name: 'Foo bar',
-      phonenumber: '000'
-    }
-  ]
+  const phonebook = {
+    name: 'Superadvanced phonebook app',
+    contacts: [
+      {
+        name: 'John Doe',
+        phonenumber: '358401234567',
+        id: 1
+      },
+      {
+        name: 'Jane Doe',
+        phonenumber: '44551234567',
+        id: 2
+      },
+      {
+        name: 'Foo bar',
+        phonenumber: '000',
+        id: 3
+      },
+    ]
+  }
 
   return (
     <div>
-      <header title={title} />
-      <contents contacts={contacts} />
+      <h1 className='header'>{phonebook.name}</h1>
+
+      {
+        phonebook.contacts.map(numberitem => (
+          <div className='numberItem' key={numberitem.id}>
+            <h3 className='name'>Name: {numberitem.name}</h3>
+            <span className='number'>
+              <h3>Number:</h3>
+              <h3>{numberitem.phonenumber}</h3>
+            </span>
+          </div>
+        ))
+      }
+      <h3 className='h3'>{}Total number of entries: 3</h3>
+      
     </div>
   )
 }
